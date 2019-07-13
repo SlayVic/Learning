75b22ef950993cea37365a4354582b58c9c532b0
JamperXD <superjamperua@gmail.com>
2019-07-13 03:00:43 +0300

    Create first stem in Castling

--- Python/Chess/GameController.py
+++ Python/Chess/GameController.py
@@ -10,48 +10,48 @@ class Controller(object):
         # for y in range(8):
         #     a = GE.Pawn(0, 1, y)
         #     self.whiteFigures.append(a)
-        # a = GE.Rook(0, 0, 0)
-        # self.whiteFigures.append(a)
+        a = GE.Rook(0, 0, 0)
+        self.whiteFigures.append(a)
         # a = GE.Knight(0, 0, 1)
         # self.whiteFigures.append(a)
         # a = GE.Bishop(0, 0, 2)
         # self.whiteFigures.append(a)
         # a = GE.Queen(0, 0, 3)
         # self.whiteFigures.append(a)
-        # a = GE.King(0, 0, 4)
-        # self.whiteFigures.append(a)
+        a = GE.King(0, 0, 4)
+        self.whiteFigures.append(a)
         # a = GE.Bishop(0, 0, 5)
         # self.whiteFigures.append(a)
         # a = GE.Knight(0, 0, 6)
         # self.whiteFigures.append(a)
-        # a = GE.Rook(0, 0, 7)
-        # self.whiteFigures.append(a)
+        a = GE.Rook(0, 0, 7)
+        self.whiteFigures.append(a)
 
-        self.whiteFigures.append(GE.Pawn(0,6,3))
+        # self.whiteFigures.append(GE.Pawn(0, 6, 3))
 
         self.blackFigures = []
-        
-        self.blackFigures.append(GE.Pawn(1,1,5))
+
+        # self.blackFigures.append(GE.Pawn(1, 1, 5))
 
         # for y in range(8):
         #     a = GE.Pawn(1, -2, y)
         #     self.blackFigures.append(a)
-        # a = GE.Rook(1, -1, 0)
-        # self.blackFigures.append(a)
+        a = GE.Rook(1, -1, 0)
+        self.blackFigures.append(a)
         # a = GE.Knight(1, -1, 1)
         # self.blackFigures.append(a)
         # a = GE.Bishop(1, -1, 2)
         # self.blackFigures.append(a)
         # a = GE.Queen(1, -1, 3)
         # self.blackFigures.append(a)
-        # a = GE.King(1, -1, 4)
-        # self.blackFigures.append(a)
+        a = GE.King(1, -1, 4)
+        self.blackFigures.append(a)
         # a = GE.Bishop(1, -1, 5)
         # self.blackFigures.append(a)
         # a = GE.Knight(1, -1, 6)
         # self.blackFigures.append(a)
-        # a = GE.Rook(1, -1, 7)
-        # self.blackFigures.append(a)
+        a = GE.Rook(1, -1, 7)
+        self.blackFigures.append(a)
 
         self.queue = 0
 
@@ -107,6 +107,41 @@ class Controller(object):
                 self.blackFigures.pop(index)
                 self.blackFigures.append(GE.Queen(1, 0, item.y))
 
+    def CheckToVoid(self, x, y):
+        return self.board.boardGraphic[x][y] == "• "
+
+    def Castling(self, moveFrom, moveTo):
+        if self.queue == 0:
+            if (moveFrom[0], moveFrom[1]) == (0,0):
+                if (moveTo[0], moveTo[1]) == (0, 3):
+                    if self.CheckToVoid(0,1) and self.CheckToVoid(0,2) and self.CheckToVoid(0,3):
+                        oponentCanMove = self.GetAllOponentPothitionToAttak()
+                        if not (0,2) in oponentCanMove and not (0,4) in oponentCanMove:
+                            moveFigureIndex = self.findFigure(moveFrom[0], moveFrom[1], self.queue)
+                            canMove = CanMove(self.whiteFigures[moveFigureIndex].x, self.whiteFigures[moveFigureIndex].y, self.board, self.GetAllOponentPothitionToAttak()).canMove
+                            if (moveTo[0], moveTo[1]) in canMove:
+                                figure = self.whiteFigures.pop(moveFigureIndex)
+                                figure.x = moveTo[0]
+                                figure.y = moveTo[1]
+                                self.whiteFigures.append(figure)
+                                kingIndex = self.findFigure(0, 4, self.queue)
+                                king = self.whiteFigures.pop(kingIndex)
+                                king.x = 0
+                                king.y = 2
+                                self.whiteFigures.append(king)
+                                return True
+                            else: return False
+                        else: return False
+                    else: return False
+                else: return False
+            elif (moveFrom[0], moveFrom[1]) == (0,7):
+                pass
+            else:
+                return False
+        else:
+            # ! Тут будет Много других значений когда дело косаеться цвета фигуры, не забуть заменить, больщенство можно взять в MoveFigure
+            pass
+
     def MoveFigure(self, fromTo):
         # try:
         moveFrom = (int(fromTo[1])-1, fromTo[0])
@@ -131,11 +166,9 @@ class Controller(object):
                     self.whiteFigures.append(figure) if self.queue == 0 \
                         else self.blackFigures.append(figure)
                     self.pawnToQuin()
-                self.queue = 1 if self.queue == 0 else 0
-                self.board.SetFigures(self.whiteFigures, self.blackFigures)
-                return True
-            else:
-                return False
+            self.queue = 1 if self.queue == 0 else 0
+            self.board.SetFigures(self.whiteFigures, self.blackFigures)
+            return True
         else:
             return False
         # except expression as ex:
@@ -175,5 +208,3 @@ control = Controller()
 while True:
     print(control.board)
     control.MoveFigure(input())
-    
-
